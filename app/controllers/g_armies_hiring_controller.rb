f0c7934fd48dfa22baec8904c9eba76508a4b6d6
Zuger Cédric <zuger.cedric@gmail.com>
2016-08-04 05:20:29 +0200

    Fixing hiring controller.

--- app/controllers/g_armies_hiring_controller.rb
+++ app/controllers/g_armies_hiring_controller.rb
@@ -4,13 +4,9 @@ class GArmiesHiringController < ApplicationController
     game_board = GGameBoard.find( params[ :g_game_board_id ] )
     ActiveRecord::Base.transaction do
       params[ :armies_to_hire ].each do |_, val|
-        army = game_board.g_game_board_armies_to_recruit.where( q: val[:q], r: val[:r] ).first_or_initialize
-        if army.class == Armies::ToRecruit && army.h_house_id == val[ :house_id ].to_i
-          if army.nb_banners != val[ :army_count ].to_i
-            army.nb_banners = val[ :army_count ].to_i
-            army.save!
-          end
-        end
+        army = game_board.g_game_board_armies_to_recruit.where( q: val[:q], r: val[:r], h_house_id: val[ :house_id ] ).first_or_initialize
+        army.nb_banners = val[ :army_count ]
+        army.save!
       end
     end
     head :ok
--- test/controllers/g_armies_hiring_controller_test.rb
+++ test/controllers/g_armies_hiring_controller_test.rb
@@ -3,11 +3,18 @@ require 'test_helper'
 class GArmiesHiringControllerTest < ActionDispatch::IntegrationTest
   setup do
     @g_game_board = create( :g_game_board )
+    @house = create( :lannister )
   end
 
-  test "should create an army recruitment entry" do
+  test "should create an army recruitment entry then update it" do
     assert_difference 'Armies::ToRecruit.count' do
-      post g_game_board_g_armies_hiring_index_url( g_game_board_id: @g_game_board.id, armies_to_hire: { 0 => { q: 1, r: 1 } } )
+      post g_game_board_g_armies_hiring_index_url( g_game_board_id: @g_game_board.id,
+        armies_to_hire: { 0 => { q: 1, r: 1, house_id: @house.id, army_count: 2 } } )
+    end
+
+    assert_no_difference 'Armies::ToRecruit.count' do
+      post g_game_board_g_armies_hiring_index_url( g_game_board_id: @g_game_board.id,
+        armies_to_hire: { 0 => { q: 1, r: 1, house_id: @house.id, army_count: 3 } } )
     end
   end
 
